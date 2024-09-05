36f56f84a759b191e9323bcc5b2619d6c3bd7f22
Pranjal095 <pranjal.prajapati35@gmail.com>
2024-09-05 22:00:19 +0530

    Required changes made

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=> observer.observe(el));
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
